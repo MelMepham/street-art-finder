4c494f564219ab8bd94aa87c3ccfc658d35c817d
Melissa Mepham <m.a.mepham@gmail.com>
2018-02-20 11:15:29 +1300

    set up file for redux

---
+++ server/server.js
@@ -0,0 +1,13 @@
+const path = require('path')
+
+
+const express = require('express')
+const bodyParser = require('body-parser')
+
+const server = express()
+
+server.use(bodyParser.json())
+server.use(express.static(path.join(__dirname, '../public')))
+
+
+module.exports = server
