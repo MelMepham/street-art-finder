560c5836610e4bcdee81d47aa0e5bab8adb9f54e
kahu2018 <fake@fake.com>
2018-02-20 19:05:51 +1300

    Debugged after rebasing

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -2,9 +2,10 @@ import {combineReducers} from 'redux'
 
 import ErrorMessage from './error-message'
 import artInfo from './artInfo'
+import add from './add'
 
 export default combineReducers({
   ErrorMessage,
-  artInfo
+  artInfo,
   add
 })
