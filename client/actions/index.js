3502db81bcd85f18240691498b21579579099ab4
kahu2018 <fake@fake.com>
2018-02-20 18:56:39 +1300

    Adding art works

--- client/actions/index.js
+++ client/actions/index.js
@@ -29,6 +29,27 @@ export function getArtInfo (artInfo) {
       .catch(err => {
         console.log({err});
         dispatch(showError(err.message))
+
+export const addNewArt= (artDetails) => {
+  console.log('action', artDetails)
+  return {
+    type: 'ADD_ART',
+    artDetails
+  }
+}
+
+export function addArt(artDetails) {
+  console.log('actions', artDetails)
+  return (dispatch) => {
+    console.log('test2')
+    return request
+      .post('/api/v1/art')
+      .send(artDetails)
+      .then(res => {
+        dispatch(addNewArt(res.body))
+      .catch(err => {
+        dispatch(err.message)
+      })
       })
     }
   }
--- db.js
+++ db.js
@@ -6,6 +6,7 @@ module.exports = {
   getArt,
   getUsers,
   getFavorites
+  postArt
 }
 
 function getArt (testDb) {
